a73cd068bd20d648bbf78c1fc0ddc5479c5f676f
Stanley Jian <jianstanley@outlook.com>
2025-03-16 17:43:20 -0700

    Attempt at fix

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -50,5 +50,5 @@ vector<int> extract_shortest_path(const vector<int>&, const vector<int>& prev, i
 void print_path(const vector<int>& v, int total) {
 	for(int i : v) cout << i << ' ';
 	cout << endl;
-	cout << "Total cost is: " << total << endl;
+	cout << "Total cost is " << total << endl;
 }
